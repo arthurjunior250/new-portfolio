484adc6e10478066558b6b0a7244901622245a95
arthur <you@example.com>
2022-08-09 14:29:22 +0200

    updated info

---
+++ assets/js/contact.js
@@ -0,0 +1,40 @@
+const names = document.getElementById("contact-name");
+const email = document.getElementById("contact-email");
+const message = document.getElementById("contact-message");
+const btnContact= document.getElementById("submit");
+
+btnContact.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  fetch(
+    "https://my-brand-endpoints.herokuapp.com/api/v1/inquiry",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        names: names.value,
+        email: email.value,
+        message:message.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      console.log(response);
+      if(response.status !== 201){
+        swal("Error","Message not sent", "error");
+      }else{
+      swal({
+        title: "Good job!",
+        text: "Message Sent!",
+        icon: "success",
+        button: "Ok!",
+      }).then(() => {
+        location.reload();
+       });}
+    })
+    .catch((error) => {
+      swal("Error", response.message, "error");
+    });
+});
